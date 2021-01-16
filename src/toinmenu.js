4b07e4299dc4813a9f95e5cdd833eccd58b78627
AnjoBotJust <77548799+AnjoBotJust@users.noreply.github.com>
2021-01-16 15:46:03 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const anjomenu = (prefix, pushname) => {
+    return `◪ *Comandos do Bot*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
